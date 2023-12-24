b1664c218ce13a6347c9e4108928385cf794f117
julia nowicka <julia.nowicka@lemondev.pl>
2023-12-24 15:21:03 +0100

    Add plus-minus solution

---
+++ hackerrank-plus-minus/plus-minus.js
@@ -0,0 +1,63 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    let positiveNumbersCount = 0;
+    let negativeNumbersCount = 0;
+    let zerosNumbersCount = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            zerosNumbersCount++;
+        } else if (arr[i] > 0) {
+            positiveNumbersCount++;
+        } else if (arr[i] < 0) {
+            negativeNumbersCount++;
+        }              
+    }
+    
+    const positiveNumbersRatio = positiveNumbersCount / arr.length;
+    const negativeNumbersRatio = negativeNumbersCount / arr.length;
+    const zerosNumbersRatio = zerosNumbersCount / arr.length;
+
+    const positiveNumbersFormatted = positiveNumbersRatio.toFixed(6);
+    const negativeNumbersFormatted = negativeNumbersRatio.toFixed(6);
+    const zerosNumbersFormatted = zerosNumbersRatio.toFixed(6);
+
+    console.log(positiveNumbersFormatted);
+    console.log(negativeNumbersFormatted);
+    console.log(zerosNumbersFormatted);   
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
