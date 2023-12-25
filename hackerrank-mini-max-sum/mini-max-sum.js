739ee97c97ce90f1c8be7d72f0f7c4d069de1995
julia nowicka <julia.nowicka@lemondev.pl>
2023-12-25 22:08:35 +0100

    Add Mini-Max Sum solution from HackerRank

---
+++ hackerrank-mini-max-sum/mini-max-sum.js
@@ -0,0 +1,56 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    let max = arr[0];
+    let min = arr[0];
+    let totalSum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentValue = arr[i];
+        totalSum += currentValue;
+        if (currentValue > max){
+             max = currentValue;
+        }
+        if (currentValue < min){
+            min = currentValue;
+        }
+    }
+
+    const minSum = totalSum - max; 
+    const maxSum = totalSum - min;
+    console.log(`${minSum} ${maxSum}`);
+
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
