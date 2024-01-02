ed718be9712f3f06bdee549fa465b09668883c16
julia nowicka <julia.nowicka@lemondev.pl>
2024-01-02 21:43:03 +0100

    Add task solution

---
+++ hackerrank-lonely-integer/lonely-integer.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    const set = new Set();
+    for (let i = 0; i < a.length; i++) {
+        const currentValue = a[i];
+        if (set.has(currentValue)) {
+            set.delete(currentValue);
+        } else {
+            set.add(currentValue);
+        }
+    }
+
+    for (let key of set.keys()) {
+        return key;
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
