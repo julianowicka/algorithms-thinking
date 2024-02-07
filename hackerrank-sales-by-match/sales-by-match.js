8891cf8fc2b61bb2ada579085bac4d7a159f5381
julia nowicka <julia.nowicka@lemondev.pl>
2024-02-07 14:03:10 +0100

    Add task solution

---
+++ hackerrank-sales-by-match/sales-by-match.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    let sockCounts = {};
+    let pairs = 0;
+
+    for (let i = 0; i < n; i++) {
+        let color = ar[i];
+        if (!sockCounts[color]) {
+            sockCounts[color] = 0;
+        }
+        sockCounts[color]++;
+    }
+
+    for (let color in sockCounts) {
+        pairs += Math.floor(sockCounts[color] / 2);
+    }
+
+    return pairs;
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
