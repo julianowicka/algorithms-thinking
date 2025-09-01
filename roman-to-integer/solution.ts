287083b211ee9ee44ddbe2339c128ec15210c596
Julia Nowicka <julia.nowicka@lemondev.pl>
2025-09-01 19:09:36 +0200

    Refactor romanToInt function by removing debug logs and improving formatting

--- roman-to-integer/solution.ts
+++ roman-to-integer/solution.ts
@@ -1,28 +1,24 @@
 function romanToInt(s: string): number {
     const romanValues: Record<string, number> = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     };
 
-    console.log("s.length", s.length)
+
+    let sumCharacter = 0;
     for (let i = 0; i < s.length; i++) {
-        console.log("i", i, "----->", s[i])
+        const currentCharacter = s[i]
+        const romanValue = romanValues[currentCharacter]
+        sumCharacter = sumCharacter + romanValue
 
     }
 
-    const keys = Object.keys(romanValues);
-    console.log("keys", keys)
-    console.log("roman values[v]", romanValues["V"])
-
-    for (let i = 0; i < keys.length; i++) {
-        console.log("keys[i]", romanValues[keys[i]])
-    }
-
+    return sumCharacter
 };
 
-console.log(romanToInt("IVLI"))
+console.log(romanToInt("LXIII"))
