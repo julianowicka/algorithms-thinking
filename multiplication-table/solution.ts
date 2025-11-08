bf87303016ea759ce361b1e53c5bbc55255b95a7
Julia Nowicka <julia.nowickaa@gmail.com>
2025-11-08 11:23:52 +0100

    Refactor print function to use template literals for output formatting

--- multiplication-table/solution.ts
+++ multiplication-table/solution.ts
@@ -1,7 +1,7 @@
 function print(n: number): void {
-  
-  for(let i = 1; i < 11; i++){
-    console.log(n, "*", i, "=", n * i)
+  for (let i = 1; i < 11; i++) {
+    console.log(`${n} * ${i} = ${n * i}`)
   }
 }
-console.log(print(5))
+
+print(2)
