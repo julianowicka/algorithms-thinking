53c1de151ba59b1be6a210a2138521397932ef78
Julia Nowicka <julia.nowickaa@gmail.com>
2025-12-26 22:56:21 +0100

    Add Sieve of Eratosthenes implementation to find all prime numbers up to a given limit

---
+++ sieve-of-eratosthenes/solution.ts
@@ -0,0 +1,28 @@
+function sieveOfEratosthenes(n: number): number[] {
+    if (n < 2) return [];
+  
+    const isPrime: boolean[] = new Array(n + 1).fill(true);
+    
+    isPrime[0] = false;
+    isPrime[1] = false;
+  
+    for (let i = 2; i * i <= n; i++) {
+      if (isPrime[i]) {
+        for (let j = i * i; j <= n; j += i) {
+          isPrime[j] = false;
+        }
+      }
+    }
+  
+    const primes: number[] = [];
+    for (let i = 2; i <= n; i++) {
+      if (isPrime[i]) {
+        primes.push(i);
+      }
+    }
+  
+    return primes;
+  }
+  
+  console.log(sieveOfEratosthenes(10));
+  console.log(sieveOfEratosthenes(35)); 
